d1eec6f7611b0ef27b6405c9accaeb888e3f562b
Tianqi Shen <1330883262@qq.com>
2025-03-13 14:44:26 -0500

    change interface

--- RISC-V Processor/immediate_execution.sv
+++ RISC-V Processor/immediate_execution.sv
@@ -1,6 +1,8 @@
 module immediate_execution(
 
-    input logic [24:0] instruction_wo_opcode,
+    input [24:0] instruction_without_opcode,
+    input [2:0] imm_ctrl_ID,
+    output logic [31:0] imm_res_ID
 );
 
 
