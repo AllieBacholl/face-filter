fe8387d9af0d1b7107d4778490b949d8fcd214c1
Tianqi Shen <1330883262@qq.com>
2025-04-06 11:51:06 -0500

    ready for test memory

--- RISC-V Processor/proc_hier_bench.sv
+++ RISC-V Processor/proc_hier_bench.sv
@@ -47,7 +47,7 @@ end
                   MemAddress,
                   MemData);
 
-         $display("INUM: %8d PC: 0x%04x REG: %d VALUE: 0x%04x rs1: 0x%04x rs2: 0x%04x rs1_data_ID: 0x%04x rs2_data_ID: 0x%04x ADDR: 0x%04x",
+         $display("INUM: %8d PC: 0x%04x REG: %d VALUE: 0x%04x rs1: 0x%04x rs2: 0x%04x rs1_data_ID: 0x%04x rs2_data_ID: 0x%04x ADDR: 0x%04x MemData: 0x%04x",
                          (inst_count-1),
                         PC,
                         WriteRegister,
@@ -56,7 +56,8 @@ end
                         rs2_ID,
                         rs1_data_ID,
                         rs2_data_ID,
-                        MemAddress);
+                        MemAddress,
+                        MemData);
          
          // $display("SIMLOG:: Cycle %d PC: %8x I: %8x R: %d %3d %8x M: %d %d %8x %8x",
          // DUT.c0.cycle_count,
