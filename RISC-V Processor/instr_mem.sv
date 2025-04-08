5817290dc96480eb09fc3f3d969c5a48f6ed3aad
jliu2247 <jliu2247@wisc.edu>
2025-04-08 17:59:33 -0500

    fix beq

--- RISC-V Processor/instr_mem.sv
+++ RISC-V Processor/instr_mem.sv
@@ -17,7 +17,7 @@ module instr_mem(
     
     // Initialize instruction memory from external hex file
     initial begin
-        $readmemh("../hex.file/itype1.hex", instr_mem);
+        $readmemh("../hex.file/beq.hex", instr_mem);
     end
     
 endmodule
